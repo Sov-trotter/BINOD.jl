16cf41883497972dfda35857376fbbaf0d719eec
ARSH SHARMA <184517@nith.ac.in>
2020-08-10 19:54:47 +0530

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,5 @@ using BINOD
 using Test
 
 @testset "BINOD.jl" begin
-        greet("
-        ____ ___ _   _  ___  ____  
-        | __ )_ _| \ | |/ _ \|  _ \ 
-        |  _ \| ||  \| | | | | | | |
-        | |_) | || |\  | |_| | |_| |
-        |____/___|_| \_|\___/|____/ 
-        ")
+    @test sprint(print, "BINOD") == "BINOD"
 end
