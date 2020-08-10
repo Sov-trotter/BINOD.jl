f9d2b92b33d13d699bddfe44676129d97b4a6c30
ARSH SHARMA <184517@nith.ac.in>
2020-08-10 19:57:47 +0530

    Bump release

--- src/BINOD.jl
+++ src/BINOD.jl
@@ -1,15 +1,3 @@
 module BINOD
-    # using Plots
-
-    module BINOD
-    # using Plots
     print("BINOD")
-
-    
-#     Base.show("""
-#     ____ ___ _   _  ___  ____  
-#    | __ )_ _| \ | |/ _ \|  _ \ 
-#    |  _ \| ||  \| | | | | | | |
-#    | |_) | || |\  | |_| | |_| |
-#    |____/___|_| \_|\___/|____/ """)
 end
